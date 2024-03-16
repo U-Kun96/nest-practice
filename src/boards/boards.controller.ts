72625d2c98ceaf220907b84a8838bb3cb2b2d0b4
유민식 <soule125@naver.com>
2024-03-16 18:32:11 +0900

    컨트롤러에 핸들러 레벨 파이프 만들어버림

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -13,28 +23,27 @@ export class BoardsController {
   }
 
   @Post()
-  createBoard(
-    @Body() createBoardDto: CreateBoardDto
-  ): Board {
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardService.createBoard(createBoardDto);
   }
 
   @Get('/:id')
   // @param() 괄호 안에 특정한 것을 통해서 가져오고 싶을 때만 넣어주면 된다.
   getBoardById(@Param('id') id: string): Board {
-    return this.boardService.getBoardById(id)
+    return this.boardService.getBoardById(id);
   }
 
   @Delete('/:id')
-  deleteBoard(@Param('id') id:string): void {
-    return this.boardService.deleteBoard(id)
+  deleteBoard(@Param('id') id: string): void {
+    return this.boardService.deleteBoard(id);
   }
-  
+
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus
-    ) {
-      return this.updateBoardStatus(id,status)
-    }
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.updateBoardStatus(id, status);
+  }
 }
