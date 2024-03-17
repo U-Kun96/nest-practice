e364a1f976b7d4edc2c55ceda30c6cd02e7442ab
유민식 <soule125@naver.com>
2024-03-17 21:29:55 +0900

    특정 게시물을 찾을 때 없는 경우 결과 값 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Delete, Injectable } from '@nestjs/common';
+import { Delete, Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -11,8 +11,8 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(createBoardDto:CreateBoardDto) {
-    const {title, description} = createBoardDto
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
 
     const board: Board = {
       id: uuid(),
@@ -24,17 +24,25 @@ export class BoardsService {
     return board;
   }
 
-  getBoardById(id:string):Board { // id로 게시물을 찾음 파라미터 = id, return 값은 하나이기 때문에 Board라고 지정해줌
-    return this.boards.find((board) => board.id === id)
+  getBoardById(id: string): Board {
+    // id로 게시물을 찾음 파라미터 = id, return 값은 하나이기 때문에 Board라고 지정해줌
+    const post = this.boards.find((board) => board.id === id);
+    // 찾고 싶은 게시글이 없을 때 에러를 던져주는 인스턴스를 생성해준다.
+    if (!post) {
+      throw new NotFoundException(
+        `${id}에 해당하는 게시글이 존재하지 않습니다.`,
+      );
+    }
+    return post;
   }
 
-  deleteBoard(id:string):void {
-    this.boards = this.boards.filter((board) => board.id !== id)
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
   }
 
-  updateBoardStatus(id: string, status:BoardStatus): Board {
+  updateBoardStatus(id: string, status: BoardStatus): Board {
     const board = this.getBoardById(id); // 업데이트 하고 싶은 게시물을 하나 찾아서 할당함
-    board.status = status
-    return board
+    board.status = status;
+    return board;
   }
 }
