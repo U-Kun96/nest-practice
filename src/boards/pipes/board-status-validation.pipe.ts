44d0f20b44c6e51aa5f9705c194a4ac486172cb9
유민식 <soule125@naver.com>
2024-03-18 00:24:59 +0900

    타입오알엠 연동시킴

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(
+        `${value}은/는 상태 옵션 값에 포함되어 있지 않습니다`,
+      );
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
